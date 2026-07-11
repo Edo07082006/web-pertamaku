14ee4a86c745dc0d17af2d3f4bc12492b42768c2
Edo07082006 <bangsmart15@gmail.com>
2026-07-11 16:06:46 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn");
+const pesan = document.getElementById("pesan");
+
+btn.onclick = function(){
+
+    pesan.innerHTML =
+    "Aku lupa cara membuka foto itu.<br><br>Mungkin nama filenya <b>kenangan.txt</b>";
+
+};
