2063dfb0249a7046e07d22aad7df006788660411
Edo07082006 <bangsmart15@gmail.com>
2026-07-11 20:38:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const pesan = document.getElementById("pesan");
 btn.onclick = function(){
 
     pesan.innerHTML =
-    "Aku lupa cara membuka foto itu.<br><br>Mungkin nama filenya <b>kenangan.txt</b>";
+    "Aku lupa cara membuka foto itu.<br><br>Mungkin nama filenya <b>kenangan.zip</b>";
 
 };
